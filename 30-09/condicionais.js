432638948866ab774764d02a629b543f55c3d1db
@AbinerPetry <luisabiner42@gmail.com>
2024-10-08 11:30:38 -0300

    Add files via upload

---
+++ 30-09/condicionais.js
@@ -0,0 +1,55 @@
+/*let idade = 60
+
+let temCarteira = false
+
+if (idade >= 18 && temCarteira) {
+
+    console.log('Você tem idade para dirigir e tem carteira')
+}
+else if (idade >= 18){
+
+    console.log('Você ate tem idade para dirigir, mas não tem carteira. Chame uber')
+}
+else {
+    console.log('você não pode dirigir')
+}
+
+let hora = 23
+
+
+if (hora < 12 && hora >= 6 ) {
+    console.log('Bom Dia!')
+}
+
+else if (hora <=23  && hora >= 18){
+    console.log('Boa Noite!')
+}
+else if (hora < 18 && hora >= 12){
+    console.log('Boa Tarde!')
+}
+else {
+    console.log('Vai Dormir!')
+}*/
+
+let jogo = ''
+
+switch(jogo){
+
+    case 'Red Dead Redemption II':
+    console.log('jogo do velho oeste')
+    break
+ 
+    case 'Skyrim':
+    console.log('melhor jogo ja feito pela humanidade')
+    break
+     
+    case 'Dark Souls':
+    console.log('meu deus, eu so morro nessa joça')
+    break
+
+    default:
+    console.log('não conheço esse jogo!')
+    break
+    
+
+}
---
+++ 30-09/loops.js
@@ -0,0 +1,9 @@
+// while
+let volta = 0 
+
+while (volta < 10)
+    console.log(`pedale,você ja deu ${volta} voltas.`)
+
+volta++
+console.log('voce ganhou a corida')
+//for
