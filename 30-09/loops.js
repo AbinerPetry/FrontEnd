f3bde6fc65139a0f03fd426b63b5ebb67f5a84b6
@AbinerPetry <luisabiner42@gmail.com>
2024-10-09 09:08:16 -0300

    Add files via upload

--- 30-09/loops.js
+++ 30-09/loops.js
@@ -1,9 +1,16 @@
 // while
 let volta = 0 
 
-while (volta < 10)
+while (volta < 10){
     console.log(`pedale,você ja deu ${volta} voltas.`)
 
 volta++
+}
 console.log('voce ganhou a corida')
+
 //for
+
+for (let voltar1 = 0; voltar1 < 10; voltar1 ++){
+    console.log(`você ja deu ${voltar1} voltas`)
+}
+console.log('parabéns, você ganhou a corrida!')
